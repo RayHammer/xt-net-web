d3ccc01c6c90de357029dfa217a70f9c22bec8b5
Yegor Kondratyev <evkondratyev@yandex.ru>
2020-02-06 14:21:09 +0400

    Submitting subtask 9.4.

--- xt-net-web/Task09/scripts/9-4.js
+++ xt-net-web/Task09/scripts/9-4.js
@@ -1,5 +1,12 @@
 (function() {
-    var slides = document.querySelectorAll("#presentation > section");
+    const countdownMax = 9;
+    const slides = document.querySelectorAll("#presentation > section");
+    const counter = document.getElementById("counter");
+    const counterText = counter.firstElementChild;
+    const goBackButton = document.getElementById("go-back");
+    var currentSlide = 0;
+    var countdown = countdownMax;
+    var counterTimer, isTicking = false;
 
     function setActiveSlide(id) {
         slides.forEach(element => {
@@ -8,5 +15,50 @@
         slides[id].style.display = "block";
     }
 
-    setActiveSlide(0);
+    function tick() {
+        countdown--;
+        if (countdown <= 0) {
+            countdown = countdownMax;
+            if (++currentSlide == slides.length) {
+                clearInterval(counterTimer);
+                if (confirm("Показ слайдов завершён. Начать заново?")) {
+                    currentSlide = 0;
+                    counterTimer = setInterval(tick, 1000);
+                } else {
+                    window.close();
+                }
+            }
+            setActiveSlide(currentSlide);
+        }
+        counterText.innerText = countdown;
+    }
+
+    function toggleTick() {
+        if (isTicking) {
+            clearInterval(counterTimer);
+        } else {
+            counterTimer = setInterval(tick, 1000);
+        }
+        isTicking = !isTicking;
+    }
+
+    function goBack() {
+        if (isTicking) {
+            countdown = countdownMax;
+            toggleTick();
+            toggleTick();
+            counterText.innerText = countdown;
+        }
+        if (currentSlide > 0) {
+            currentSlide--;
+            setActiveSlide(currentSlide);
+        }
+    }
+
+    setActiveSlide(currentSlide);
+    counterText.innerText = countdown;
+    toggleTick();
+
+    counter.onclick = toggleTick;
+    goBackButton.onclick = goBack;
 })()
