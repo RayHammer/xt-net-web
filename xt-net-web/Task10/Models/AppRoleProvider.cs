08afecc0e839482276c91ad4a4b9d0a1a065c126
Yegor Kondratyev <evkondratyev@yandex.ru>
2020-02-23 20:57:25 +0400

    Added basic security.

---
+++ xt-net-web/Task10/Models/AppRoleProvider.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Task10.Models
+{
+    public class AppRoleProvider : RoleProvider
+    {
+        private readonly Dictionary<string, List<string>> roles = new Dictionary<string, List<string>>();
+
+        public AppRoleProvider()
+        {
+            roles["admin"] = new List<string>()
+            {
+                "User", "Admin"
+            };
+            roles["user"] = new List<string>()
+            {
+                "User"
+            };
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            return roles.ContainsKey(username) && roles[username].Contains(roleName);
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            return roles.ContainsKey(username) ? roles[username].ToArray() : Array.Empty<string>();
+        }
+
+        public bool ContainsUser(string username)
+        {
+            return roles.ContainsKey(username);
+        }
+
+        public bool RegisterUser(string username)
+        {
+            if (ContainsUser(username))
+                return false;
+            roles[username] = new List<string>() { "User" };
+            return true;
+        }
+
+        #region NOT_IMPLEMENTED
+        public override string ApplicationName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ xt-net-web/Task10/Models/AuthModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task10.Models
+{
+    public static class AuthModel
+    {
+        public static Dictionary<string, string> AuthList
+        {
+            get; private set;
+        } = new Dictionary<string, string>();
+
+        public static bool LoginSuccessful(string username, string password)
+        {
+            return password == "admin";
+        }
+    }
+}
